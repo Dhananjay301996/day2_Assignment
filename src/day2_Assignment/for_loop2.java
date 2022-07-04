64081af1ee3cd0112b44a7793c3612d143e1b79c
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-07-04 18:22:10 +0530

    Added for loop program 2

---
+++ src/day2_Assignment/for_loop2.java
@@ -0,0 +1,23 @@
+package day2_Assignment;
+import java.util.*;
+
+public class for_loop2 {
+
+	public static void main(String[] args) {
+		System.out.println("enter your number");
+
+		int reverse = 0;
+		int remainder = 0;
+		Scanner sc = new Scanner(System.in);
+		
+
+		for (int n = sc.nextInt(); n > 0; n = n / 10) {
+			remainder = n % 10;
+			reverse = reverse * 10 + remainder;
+		}
+
+		System.out.println("The reverse number is " + reverse);
+
+	}
+
+}
